fccd0250a1ad613b1f85ed0307d5fa52a3e1ff5e
hellenvelasques <hgv92jc@gmail.com>
2023-09-30 09:19:15 -0300

    feat: instalação do cors;

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 require("express-async-errors");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
+const cors = require("cors");
 
 const express = require("express");
 const routes = require("./routes");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
-
 app.use(routes);
 
 app.use(( error, request, response, next) => {
